8734ee688471c370a48d05be87467cfbb9b8d87b
sean <seanjiangsh@gmail.com>
2023-04-11 19:30:18 +0800

    Designing an e-commerce schema

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,51 @@ import { graphqlHTTP } from "express-graphql";
 
 const schema = buildSchema(`
   type Query {
-    description: String
-    price: Float
+    products: [Product]
+    orders: [Order]
+  }
+
+  type Product {
+    id: ID!
+    description: String!
+    reviews: [Review]
+    price: Float!
+  }
+
+  type Review {
+    rating: Int!
+    comment: String
+  }
+
+  type Order {
+    data: String!
+    subtotal: Float!
+    items: [OrderItem]
+  }
+
+  type OrderItem {
+    product: Product!
+    quantity: Int!
   }
 `);
 
 const rootValue = {
-  description: "Red Shoe",
-  price: 42.12,
+  products: [
+    { id: "redshoe", description: "Red Shoe", price: 42.12 },
+    { id: "redshbulejeanoe", description: "Blue Jeans", price: 55.55 },
+  ],
+  orders: [
+    {
+      date: "2005-05-05",
+      subtotal: 90.22,
+      items: [
+        {
+          product: { id: "redshoe", description: "Old Red Shoe", price: 45.11 },
+          quantity: 2,
+        },
+      ],
+    },
+  ],
 };
 
 const app = express();
