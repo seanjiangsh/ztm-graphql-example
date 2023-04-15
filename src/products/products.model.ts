4f837b86ee9129abc97c805eafb80d8176223242
sean <seanjiangsh@gmail.com>
2023-04-15 20:23:53 +0800

    Implementing Mutations

--- src/products/products.model.ts
+++ src/products/products.model.ts
@@ -1,6 +1,16 @@
-const products = [
-  { id: "redshoe", description: "Red Shoe", price: 42.12 },
-  { id: "bulejean", description: "Blue Jeans", price: 55.55 },
+type Product = {
+  id: string;
+  description: string;
+  price: number;
+  reviews: Array<Review>;
+};
+type Review = {
+  rating: number;
+  comment?: string;
+};
+const products: Array<Product> = [
+  { id: "redshoe", description: "Red Shoe", price: 42.12, reviews: [] },
+  { id: "bulejean", description: "Blue Jeans", price: 55.55, reviews: [] },
 ];
 
 function getAllProducts() {
@@ -15,4 +25,29 @@ function getProductsById(id: string) {
   return products.find((p) => p.id === id);
 }
 
-export { getAllProducts, getProductsByPrice, getProductsById };
+function addNewProduct(id: string, description: string, price: number) {
+  // * return undefined if product exist
+  if (getProductsById(id)) return;
+
+  const newProduct: Product = { id, description, price, reviews: [] };
+  products.push(newProduct);
+  return newProduct;
+}
+
+function addNewProductReview(id: string, rating: number, comment?: string) {
+  // * return undefined if product not exist
+  const targetProduct = getProductsById(id);
+  if (!targetProduct) return;
+
+  const newReview: Review = { rating, comment };
+  targetProduct.reviews.push(newReview);
+  return targetProduct;
+}
+
+export {
+  getAllProducts,
+  getProductsByPrice,
+  getProductsById,
+  addNewProduct,
+  addNewProductReview,
+};
--- src/products/products.resolvers.ts
+++ src/products/products.resolvers.ts
@@ -10,6 +10,22 @@ const productResolvers = {
     productsById: (parent: any, args: { id: string }) =>
       productsModel.getProductsById(args.id),
   },
+  Mutation: {
+    addNewProduct: (
+      parent: any,
+      args: { id: string; description: string; price: number }
+    ) => {
+      const { id, description, price } = args;
+      return productsModel.addNewProduct(id, description, price);
+    },
+    addNewProductReview: (
+      parent: any,
+      args: { id: string; rating: number; comment?: string }
+    ) => {
+      const { id, rating, comment } = args;
+      return productsModel.addNewProductReview(id, rating, comment);
+    },
+  },
 };
 
 export default productResolvers;
