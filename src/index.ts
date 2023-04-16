b6dfaf35b0dbe7824e187d8345dd4ff011ee651a
sean <seanjiangsh@gmail.com>
2023-04-16 18:52:40 +0800

    Changed to apollo-server-express

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import express from "express";
 
-import { graphqlHTTP } from "express-graphql";
+import { ApolloServer } from "apollo-server-express";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { loadFilesSync } from "@graphql-tools/load-files";
 
 const typeDefs = loadFilesSync("**/*", { extensions: ["graphql"] });
 const resolvers = loadFilesSync("**/*", { extensions: ["resolvers.ts"] });
-const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-const app = express();
-app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
+async function startApolloServer() {
+  const app = express();
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+  const server = new ApolloServer({ schema });
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
 
-app.listen(5000, () => {
-  console.log("GraphQL server running on 5000");
-});
+  app.listen(5000, () => {
+    console.log("GraphQL server running on 5000");
+  });
+}
+
+startApolloServer();
